87341c215485b595d29817fdff70a6c117955c0e
unknown <andjelavostic@gmail.com>
2026-05-22 12:45:48 +0200

    [ADD] packages and user assessment class

---
+++ projekat/model/src/main/java/com/ftn/sbnz/model/assessment/UserAssessment.java
@@ -0,0 +1,347 @@
+package com.ftn.sbnz.model.assessment;
+
+public class UserAssessment {
+    // Emotional
+    private double stressLevel;
+    private double emotionalExhaustion;
+    private double nervousness;
+    private double overloadFeeling;
+    private double lowMoodFrequency;
+    private double moodSwings;
+    private double sadnessLevel;
+    private double lossOfControlFeeling;
+    private double irritability;
+    private double worryFrequency;
+
+    // Sleep / physical
+    private double sleepHours;
+    private double sleepProblems;
+    private double nightAwakenings;
+    private double restedAfterSleep;
+    private double chronicFatigue;
+    private double stressHeadaches;
+    private double appetiteChanges;
+    private double lowEnergy;
+    private double physicalExhaustion;
+
+    // Cognitive
+    private double concentrationProblems;
+    private double forgetfulness;
+    private double decisionDifficulty;
+    private double mentalConfusion;
+    private double productivityDrop;
+    private double attentionSpanIssues;
+    private double mentalFatigue;
+
+    // Social
+    private double socialAvoidance;
+    private double communicationWithdrawal;
+    private double socialIsolationFeeling;
+    private double lossOfInterest;
+    private double familyAvoidance;
+    private double timeSpentAlone;
+    private double emotionalDistance;
+
+    // Stressors
+    private double workPressure;
+    private double financialProblems;
+    private double recentStressEvent;
+    private double relationshipIssues;
+    private double lackOfRestTime;
+    private double constantPressure;
+    private double ruminationOnTasks;
+
+    // Temporal
+    private double symptomDuration;
+    private double isolationDuration;
+    private double exhaustionDuration;
+    private double panicFrequency;
+    private double stressTrend;
+    private double moodDegradationTrend;
+    private double productivityDeclineTrend;
+    public double getStressLevel() {
+        return stressLevel;
+    }
+    public void setStressLevel(double stressLevel) {
+        this.stressLevel = stressLevel;
+    }
+    public double getEmotionalExhaustion() {
+        return emotionalExhaustion;
+    }
+    public void setEmotionalExhaustion(double emotionalExhaustion) {
+        this.emotionalExhaustion = emotionalExhaustion;
+    }
+    public double getNervousness() {
+        return nervousness;
+    }
+    public void setNervousness(double nervousness) {
+        this.nervousness = nervousness;
+    }
+    public double getOverloadFeeling() {
+        return overloadFeeling;
+    }
+    public void setOverloadFeeling(double overloadFeeling) {
+        this.overloadFeeling = overloadFeeling;
+    }
+    public double getLowMoodFrequency() {
+        return lowMoodFrequency;
+    }
+    public void setLowMoodFrequency(double lowMoodFrequency) {
+        this.lowMoodFrequency = lowMoodFrequency;
+    }
+    public double getMoodSwings() {
+        return moodSwings;
+    }
+    public void setMoodSwings(double moodSwings) {
+        this.moodSwings = moodSwings;
+    }
+    public double getSadnessLevel() {
+        return sadnessLevel;
+    }
+    public void setSadnessLevel(double sadnessLevel) {
+        this.sadnessLevel = sadnessLevel;
+    }
+    public double getLossOfControlFeeling() {
+        return lossOfControlFeeling;
+    }
+    public void setLossOfControlFeeling(double lossOfControlFeeling) {
+        this.lossOfControlFeeling = lossOfControlFeeling;
+    }
+    public double getIrritability() {
+        return irritability;
+    }
+    public void setIrritability(double irritability) {
+        this.irritability = irritability;
+    }
+    public double getWorryFrequency() {
+        return worryFrequency;
+    }
+    public void setWorryFrequency(double worryFrequency) {
+        this.worryFrequency = worryFrequency;
+    }
+    public double getSleepHours() {
+        return sleepHours;
+    }
+    public void setSleepHours(double sleepHours) {
+        this.sleepHours = sleepHours;
+    }
+    public double getSleepProblems() {
+        return sleepProblems;
+    }
+    public void setSleepProblems(double sleepProblems) {
+        this.sleepProblems = sleepProblems;
+    }
+    public double getNightAwakenings() {
+        return nightAwakenings;
+    }
+    public void setNightAwakenings(double nightAwakenings) {
+        this.nightAwakenings = nightAwakenings;
+    }
+    public double getRestedAfterSleep() {
+        return restedAfterSleep;
+    }
+    public void setRestedAfterSleep(double restedAfterSleep) {
+        this.restedAfterSleep = restedAfterSleep;
+    }
+    public double getChronicFatigue() {
+        return chronicFatigue;
+    }
+    public void setChronicFatigue(double chronicFatigue) {
+        this.chronicFatigue = chronicFatigue;
+    }
+    public double getStressHeadaches() {
+        return stressHeadaches;
+    }
+    public void setStressHeadaches(double stressHeadaches) {
+        this.stressHeadaches = stressHeadaches;
+    }
+    public double getAppetiteChanges() {
+        return appetiteChanges;
+    }
+    public void setAppetiteChanges(double appetiteChanges) {
+        this.appetiteChanges = appetiteChanges;
+    }
+    public double getLowEnergy() {
+        return lowEnergy;
+    }
+    public void setLowEnergy(double lowEnergy) {
+        this.lowEnergy = lowEnergy;
+    }
+    public double getPhysicalExhaustion() {
+        return physicalExhaustion;
+    }
+    public void setPhysicalExhaustion(double physicalExhaustion) {
+        this.physicalExhaustion = physicalExhaustion;
+    }
+    public double getConcentrationProblems() {
+        return concentrationProblems;
+    }
+    public void setConcentrationProblems(double concentrationProblems) {
+        this.concentrationProblems = concentrationProblems;
+    }
+    public double getForgetfulness() {
+        return forgetfulness;
+    }
+    public void setForgetfulness(double forgetfulness) {
+        this.forgetfulness = forgetfulness;
+    }
+    public double getDecisionDifficulty() {
+        return decisionDifficulty;
+    }
+    public void setDecisionDifficulty(double decisionDifficulty) {
+        this.decisionDifficulty = decisionDifficulty;
+    }
+    public double getMentalConfusion() {
+        return mentalConfusion;
+    }
+    public void setMentalConfusion(double mentalConfusion) {
+        this.mentalConfusion = mentalConfusion;
+    }
+    public double getProductivityDrop() {
+        return productivityDrop;
+    }
+    public void setProductivityDrop(double productivityDrop) {
+        this.productivityDrop = productivityDrop;
+    }
+    public double getAttentionSpanIssues() {
+        return attentionSpanIssues;
+    }
+    public void setAttentionSpanIssues(double attentionSpanIssues) {
+        this.attentionSpanIssues = attentionSpanIssues;
+    }
+    public double getMentalFatigue() {
+        return mentalFatigue;
+    }
+    public void setMentalFatigue(double mentalFatigue) {
+        this.mentalFatigue = mentalFatigue;
+    }
+    public double getSocialAvoidance() {
+        return socialAvoidance;
+    }
+    public void setSocialAvoidance(double socialAvoidance) {
+        this.socialAvoidance = socialAvoidance;
+    }
+    public double getCommunicationWithdrawal() {
+        return communicationWithdrawal;
+    }
+    public void setCommunicationWithdrawal(double communicationWithdrawal) {
+        this.communicationWithdrawal = communicationWithdrawal;
+    }
+    public double getSocialIsolationFeeling() {
+        return socialIsolationFeeling;
+    }
+    public void setSocialIsolationFeeling(double socialIsolationFeeling) {
+        this.socialIsolationFeeling = socialIsolationFeeling;
+    }
+    public double getLossOfInterest() {
+        return lossOfInterest;
+    }
+    public void setLossOfInterest(double lossOfInterest) {
+        this.lossOfInterest = lossOfInterest;
+    }
+    public double getFamilyAvoidance() {
+        return familyAvoidance;
+    }
+    public void setFamilyAvoidance(double familyAvoidance) {
+        this.familyAvoidance = familyAvoidance;
+    }
+    public double getTimeSpentAlone() {
+        return timeSpentAlone;
+    }
+    public void setTimeSpentAlone(double timeSpentAlone) {
+        this.timeSpentAlone = timeSpentAlone;
+    }
+    public double getEmotionalDistance() {
+        return emotionalDistance;
+    }
+    public void setEmotionalDistance(double emotionalDistance) {
+        this.emotionalDistance = emotionalDistance;
+    }
+    public double getWorkPressure() {
+        return workPressure;
+    }
+    public void setWorkPressure(double workPressure) {
+        this.workPressure = workPressure;
+    }
+    public double getFinancialProblems() {
+        return financialProblems;
+    }
+    public void setFinancialProblems(double financialProblems) {
+        this.financialProblems = financialProblems;
+    }
+    public double getRecentStressEvent() {
+        return recentStressEvent;
+    }
+    public void setRecentStressEvent(double recentStressEvent) {
+        this.recentStressEvent = recentStressEvent;
+    }
+    public double getRelationshipIssues() {
+        return relationshipIssues;
+    }
+    public void setRelationshipIssues(double relationshipIssues) {
+        this.relationshipIssues = relationshipIssues;
+    }
+    public double getLackOfRestTime() {
+        return lackOfRestTime;
+    }
+    public void setLackOfRestTime(double lackOfRestTime) {
+        this.lackOfRestTime = lackOfRestTime;
+    }
+    public double getConstantPressure() {
+        return constantPressure;
+    }
+    public void setConstantPressure(double constantPressure) {
+        this.constantPressure = constantPressure;
+    }
+    public double getRuminationOnTasks() {
+        return ruminationOnTasks;
+    }
+    public void setRuminationOnTasks(double ruminationOnTasks) {
+        this.ruminationOnTasks = ruminationOnTasks;
+    }
+    public double getSymptomDuration() {
+        return symptomDuration;
+    }
+    public void setSymptomDuration(double symptomDuration) {
+        this.symptomDuration = symptomDuration;
+    }
+    public double getIsolationDuration() {
+        return isolationDuration;
+    }
+    public void setIsolationDuration(double isolationDuration) {
+        this.isolationDuration = isolationDuration;
+    }
+    public double getExhaustionDuration() {
+        return exhaustionDuration;
+    }
+    public void setExhaustionDuration(double exhaustionDuration) {
+        this.exhaustionDuration = exhaustionDuration;
+    }
+    public double getPanicFrequency() {
+        return panicFrequency;
+    }
+    public void setPanicFrequency(double panicFrequency) {
+        this.panicFrequency = panicFrequency;
+    }
+    public double getStressTrend() {
+        return stressTrend;
+    }
+    public void setStressTrend(double stressTrend) {
+        this.stressTrend = stressTrend;
+    }
+    public double getMoodDegradationTrend() {
+        return moodDegradationTrend;
+    }
+    public void setMoodDegradationTrend(double moodDegradationTrend) {
+        this.moodDegradationTrend = moodDegradationTrend;
+    }
+    public double getProductivityDeclineTrend() {
+        return productivityDeclineTrend;
+    }
+    public void setProductivityDeclineTrend(double productivityDeclineTrend) {
+        this.productivityDeclineTrend = productivityDeclineTrend;
+    }
+    
+    
+    
+}
